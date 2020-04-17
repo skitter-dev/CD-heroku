677d8f4c4c60de30e8808cea7c9859ba62043f45
skitter-dev <61031989+skitter-dev@users.noreply.github.com>
2020-04-16 22:19:23 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Anderson Araujo!"
+    return "Olá mundo, eu sou Anderson Araujo!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
